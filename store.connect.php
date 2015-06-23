0a974030eda5ca81b08e6c66bf2fcf15f0113ff4
Tommy Fisher <tommybfisher@gmail.com>
2015-06-23 16:29:08 +0300

    Saves, updates and deletes templates to mysql db

--- store.connect.php
+++ store.connect.php
@@ -1,9 +1,14 @@
 <?php
+
 $data = file_get_contents("php://input");
 $obj_data = json_decode($data);
 
 include_once 'store.connect.credentials.php';
 
-ini_set('display_errors', $obj_data->is_test ? 1 : 0);
+ini_set('display_errors', 1);
 
 $mysqli = new mysqli($host, $user, $pass, 'd24');
+
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
--- store.templates.php
+++ store.templates.php
@@ -1,7 +1,79 @@
 <?php
-$fetch_collection = 'templates';
+
 include_once 'store.connect.php';
 
-/* Set up the return array */
-$success = array();
-$success['msg'] = array();
+// Setup the return response
+$response = array();
+
+if (!isset($obj_data->statSharedID) && !isset($obj_data->template->statSharedID)) {
+    save($obj_data->template);
+} else {
+    if (!isset($obj_data->template)) {
+        remove($obj_data->statSharedID);
+    } else {
+        update($obj_data->template);
+    }
+}
+
+function save($template) {
+    global $mysqli;
+
+    $query = "INSERT INTO search_templates (template, title, space, url, secure) VALUES(?, ?, ?, ?, ?)";
+    $statement = $mysqli->prepare($query);
+    $statement->bind_param('sssss',
+       $template->template,
+       $template->title,
+       $template->space,
+       $template->url,
+       $template->secure
+    );
+    if ($statement->execute()) {
+        $response['statSharedID'] = $statement->insert_id;
+    } else {
+        $response['msg'] = 'Error: ('. $mysqli->errno .') '. $mysqli->error;
+    }
+    $statement->close();
+
+    echo json_encode($response);
+}
+
+function remove($id) {
+    global $mysqli;
+
+    $query = "DELETE FROM search_templates WHERE id = ?";
+    $statement = $mysqli->prepare($query);
+    $statement->bind_param('i', $id);
+
+    if ($statement->execute()) {
+        $response['msg'] = 'Successfully deleted id ' . $id;
+    } else {
+        $response['msg'] = 'Error: ('. $mysqli->errno .') '. $mysqli->error;
+    }
+    $statement->close();
+
+    echo json_encode($response);
+}
+
+function update($template) {
+    global $mysqli;
+
+    $query = "UPDATE search_templates SET modified=NOW(), template=?, title=?, space=?, url=?, secure=? WHERE ID=?";
+    $statement = $mysqli->prepare($query);
+    $statement->bind_param('sssssi',
+        $template->template,
+        $template->title,
+        $template->space,
+        $template->url,
+        $template->secure,
+        $template->statSharedID
+    );
+
+    if ($statement->execute()) {
+        $response['msg'] = 'Successfully updated id ' . $id;
+    } else {
+        $response['msg'] = 'Error: ('. $mysqli->errno .') '. $mysqli->error;
+    }
+    $statement->close();
+
+    echo json_encode($response);
+}
