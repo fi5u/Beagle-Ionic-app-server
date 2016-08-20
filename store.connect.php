99e1585434a72a018f502aca04e37bc52efcdf72
Tommy Fisher <tommybfisher@gmail.com>
2016-08-20 20:14:41 +0300

    Removed unused code

--- store.connect.php
+++ store.connect.php
@@ -11,6 +11,5 @@ $collection = $db->tracking;
 
 $input = file_get_contents('php://input');
 //$input = '[{"type": "event", "user": "sdj2j32mfls2mldm", "event": "auto url search", "value": "http://www.google.com/", "timestamp": "2016-05-02T19:19:52.731Z"}]';
-//$input = '{"template":"www.houseoffraser.co.uk/on/demandware.store/Sites-hof-Site/default/Search-Show?q=[?]","title":"House of Fraser™ | Clothes, Fashion, Beauty, Home & Electronics","space":"%20","secure":"false","url":"www.houseoffraser.co.uk","statSharedID":null}';
 
 $data = json_decode($input);
